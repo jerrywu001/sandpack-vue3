99225a7c3d28e5a44b4188435b601b61f8603231
jerrywu <jerrywu001@gmail.com>
2022-05-12 14:11:03 +0800

    Merge branch 'dev' of github.com:jerrywu001/sandpack-vue3 into dev

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import routes from 'virtual:generated-pages';
 
-import SanpackPlugin from '@codesandbox/sandpack-vue3';
-
 import App from './App.vue';
 
 import '@unocss/reset/tailwind.css';
@@ -12,7 +10,8 @@ import 'uno.css';
 import './styles/main.css';
 import './index.css';
 
-import '@codesandbox/sandpack-vue3/styles/index.css';
+// eslint-disable-next-line import/no-relative-packages
+import '../../src/styles/index.css';
 
 const app = createApp(App);
 const router = createRouter({
@@ -20,5 +19,4 @@ const router = createRouter({
   routes,
 });
 app.use(router);
-app.use(SanpackPlugin());
 app.mount('#app');
