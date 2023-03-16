9291f950fdddcc8f4ef7986e2aa51fe6254455cc
jerrywu001 <57242263@163.com>
2023-03-16 16:29:00 +0800

    docs: update plugin

--- scripts/after-build.ts
+++ scripts/after-build.ts
@@ -1,9 +1,11 @@
+import type { Plugin, ResolvedConfig } from 'vite';
+
 /**
  * vite build finished, and emited asstets
  * @param callback (config) => void
  */
-export default function CopyDts(callback: (config) => void) {
-  let config;
+export default function CopyDts(callback: (config: ResolvedConfig) => void): Plugin {
+  let config: ResolvedConfig;
 
   return {
     name: 'copy-dts',
